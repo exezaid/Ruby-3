95e0d5d7cbb67e9e9acdb96f820d2dcdaa91f7d9
Ezequiel <exe.zaid@gmail.com>
2010-09-29 01:52:04 -0300

    tags

--- blog/config/initializers/secret_token.rb
+++ blog/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '0023f6a972c9a094e260193cc5b71ac452e8c9e452609f3d99afef4a7348e77201a0933d5d7c66533a7c01fb923385351ab94792961a939ce6002e4c0fc1131d'
+Blog::Application.config.secret_token = '40c53e69e709409c96f72d8541825987315cad12774c92853568258d4dff66fe2e1c17847716046fb0e0c7a1411f12a42cf19dc5a0fa0c370e1db95bc6191eae'
