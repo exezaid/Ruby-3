ba19d115d3bfbb551a3902d75b412691ab50c419
clonexezaid <yo@ezequielmarquez.com.ar>
2010-09-30 18:49:02 -0700

    pequeño cambio

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate
     authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == 'admin' && password == 'password'
+      user_name == 'admin' && password == 'admin'
     end
   end
 
